1c352e6c3100dd94086d419fd4769c9b4b0681c6
linelie00 <78725815+linelie00@users.noreply.github.com>
2024-01-08 11:15:34 +0900

    빌어먹을 오타

---
+++ 5063.py
@@ -0,0 +1,6 @@
+h = int(input())
+for i in range(h):
+    r,e,c = map(int,input().split())
+    if e - c > r: print("advertise")
+    elif e - c < r: print("do not advertise")
+    else: print("does not matter")
