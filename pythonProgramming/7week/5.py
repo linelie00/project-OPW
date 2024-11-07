b1a398d44874fa8448730e214b58a77441acdfc3
linelie00 <dingpawn@gmail.com>
2024-11-07 13:05:23 +0900

    7주차 5번 문제 수정

--- pythonProgramming/7week/5.py
+++ pythonProgramming/7week/5.py
@@ -1,28 +1,3 @@
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
 #차례대로 출력
 print("대각선 요소 출력 :", [matrix[i][i] for i in range(3)])
-print(0 or "파이썬")
-print(2==2.0)
-print(2 is 2.0)
-print(5.5//2)
-print("%7.3f" % 123.45)
-print('print(bool(1))')
-print(bool(1))
-
-for i in range(0,10,2) :
-    print(i)
-
-x = y = z = 100
-print(z)
-
-x=[5,3,1,2,7,8]
-x = sorted(list(set(x)))
-print(x)
-
-l=[(x, x**2) for x in range(1,3)]
-print(l)
-
-a = "hello"
-b  = 3
-print(a * 3)
-print(5.5//2)
