6f027a4cfb51f4c7e2aea2473c9ed5dcf97cf9a8
linelie00 <san00@cu.ac.kr>
2026-02-07 20:42:09 +0900

    class 1 문제

--- 10250.py
+++ 10250.py
@@ -1,6 +1,6 @@
 t = int(input())
 for _ in range(t):
-    h, w, n = map(int, input().split())
-    y = (n - 1) % h + 1     # 층
-    x = (n - 1) // h + 1    # 호수
+    h,w,n = map(int, input().split())
+    y = (n - 1) % h + 1
+    x = (n - 1) // h + 1
     print(y * 100 + x)
