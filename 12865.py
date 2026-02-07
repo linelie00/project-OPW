1f0f02ae9300ae2c419f59891d1b8f75af8716d9
linelie00 <san00@cu.ac.kr>
2026-02-07 20:40:02 +0900

    현재 무게를 만들 때 가치가 큰 것 선택하기

--- 12865.py
+++ 12865.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 n,k = map(int,input().split())
 bag = [list(map(int,input().split())) for _ in range(n)]
-dp = [0]*(k+1) #각 무게를 가질 때의 최댓값
+dp = [0]*(k+1) #각 무게 넘지 않게 담았을 때 얻을 수 있는 최댓가치
 
 for w,v in bag:
     for weight in range(k, w - 1, -1): #k부터 w까지 -1
-        dp[weight] = max(dp[weight], dp[weight - w] + v) 
+        dp[weight] = max(dp[weight], dp[weight - w] + v) #현재 무게를 만들 때 가치가 큰 것 선택
 print(dp[k])
