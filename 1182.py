cca7bbc3e163e4fe936d6a4b7f25ab6bbe038fe6
linelie00 <san00@cu.ac.kr>
2026-02-09 23:12:06 +0900

    combinations으로 조합찾기

--- 1182.py
+++ 1182.py
@@ -1,13 +1,15 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
-n,s = map(int, input().split())
+n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
+for i in range(1, n+1):
+    comb = combinations(arr, i)
 
-for i in range(n):
-    for j in range(i,n):
-        print(i,j)
-        if sum(arr[i:j+1]) == s:
+    for x in comb:
+        if sum(x) == s:
             cnt += 1
+
 print(cnt)
