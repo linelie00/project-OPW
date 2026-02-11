dac5553118ae5b134527bb2c05ecef3df60bfb7b
linelie00 <san00@cu.ac.kr>
2026-02-11 14:05:35 +0900

    dp 규칙찾기

---
+++ 9461.py
@@ -0,0 +1,12 @@
+T = int(input())
+N = [int(input()) for _ in range(T)]
+m = max(N)
+dp = [0]*m
+dp[0],dp[1],dp[2] = 1,1,1
+
+for i in range(3, m):
+    dp[i] = dp[i-2]+dp[i-3]
+
+print(dp)
+for i in N:
+    print(dp[i-1])
