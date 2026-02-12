82615bf612dd8e8182210ec0e3c6533c7d91e103
linelie00 <san00@cu.ac.kr>
2026-02-12 11:28:12 +0900

    끝나는 시간만이 진리 그리디

---
+++ 1931.py
@@ -0,0 +1,11 @@
+N = int(input())
+meetings = [list(map(int,input().split())) for _ in range(N)]
+meetings.sort(key=lambda x:(x[1], x[0]))
+cnt = 0
+end_time = 0
+
+for (start, end) in meetings:
+    if start >= end_time:
+        end_time = end
+        cnt += 1
+print(cnt)
