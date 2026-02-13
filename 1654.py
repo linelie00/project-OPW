9ac0157ca6b28292a8dcce6f4200c73bc020a3ca
linelie00 <san00@cu.ac.kr>
2026-02-13 13:38:08 +0900

    이분탐색의 정석 <=으로 해야함

---
+++ 1654.py
@@ -0,0 +1,14 @@
+K,N = map(int,input().split())
+lan = [int(input()) for _ in range(K)]
+left,right = 1, max(lan)
+
+while left < right:
+    mid = (left+right)//2
+    lines = 0
+    for x in lan:
+        lines += x//mid
+    if lines >= N:
+        left = mid+1
+    else:
+        right = mid-1
+print(left,right)
