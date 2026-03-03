beaf105882d5fa5193c02f85d4d1a3080bb8c54e
linelie00 <san00@cu.ac.kr>
2026-03-03 20:44:33 +0900

    dp를 이용한 LIS 알고리즘

---
+++ 11053.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = list(map(int,input().split()))
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i] and dp[i] < dp[j] + 1:
+            dp[i] = dp[j] + 1
+        print(i,dp)
+print(dp[-1])
